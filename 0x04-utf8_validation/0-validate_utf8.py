2393539eef1bb7f5c22fadcdbeb854aea32d69c2
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-19 14:59:37 +0100

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module defines the validUTF8 function."""
+from List import typing
+
+
+def validUTF8(data: List[int]) -> bool:
+    """Method that determines if a given
+    data set represents a valid UTF-8 encoding."""
+    pass
