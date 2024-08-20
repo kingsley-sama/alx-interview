3a8cfd0c9e2d856bbe4fe0a091a12a599a7d2b47
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-20 18:11:54 +0100

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,12 +6,11 @@ from typing import List
 def validUTF8(data: List[int]) -> bool:
     """Method that determines if a given
     data set represents a valid UTF-8 encoding."""
-    if data:
-        try:
-            by_list = bytes(data)
-            by_list.decode('utf-8')
-            return True
-        except ValueError:
-            return False
-        except UnicodeDecodeError:
-            return False
+    try:
+        by_list = bytes(data)
+        by_list.decode('utf-8')
+        return True
+    except ValueError:
+        return False
+    except UnicodeDecodeError:
+        return False
