f5720167ede91494ecabe8a6d5c574948e3f8332
Kingsley Okpo <144631176+kingsley-sama@users.noreply.github.com>
2024-10-29 18:27:09 +0100

    Update 0-validate_utf8.py
    removes type annotation in 0-validate_ut8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """This module defines the validUTF8 function."""
-from typing import List
 
 
-def validUTF8(data: List[int]) -> bool:
+def validUTF8(data):
     """Method that determines if a given
     data set represents a valid UTF-8 encoding."""
     try:
